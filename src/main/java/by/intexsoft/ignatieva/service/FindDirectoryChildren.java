aa4126526fdd99ba0b0311fe92c912654658b9f8
margarita-ignatieva <ignatevamargarita950@gmail.com>
2021-01-28 21:04:02 +0300

    fixed method for directory fill

--- src/main/java/by/intexsoft/ignatieva/service/FindDirectoryChildren.java
+++ src/main/java/by/intexsoft/ignatieva/service/FindDirectoryChildren.java
@@ -6,23 +6,15 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class FindDirectoryChildren {
+
     public static void fillInChildrenDirectories(List<Directory> allDirectories) {
-        Map<Long, List<Directory>> directoriesMap = new HashMap<>();
-        for(Directory directory : allDirectories) {
-            long parentId = directory.getParentId();
-            directoriesMap.put(parentId, findChildrenDirectories(parentId, allDirectories));
-        }
+        Map<Long, List<Directory>> directoriesMap = allDirectories.stream()
+                .collect(Collectors.groupingBy(Directory::getParentId));
         for(Directory directory : allDirectories) {
             if (directoriesMap.containsKey(directory.getId())) {
                 directory.setChildren(directoriesMap.get(directory.getId()));
             }
         }
-    }
-
-
-    private static List<Directory> findChildrenDirectories(Long parentId, List<Directory> allDirectories) {
-        return allDirectories.stream().filter(directory -> directory.getParentId() == parentId)
-                .collect(Collectors.toList());
     }
 
 }
